c82e17f25bacfc52c12e8c00c12e39ec74de0b64
Arturo <98308604+RockyBot21@users.noreply.github.com>
2026-06-27 13:33:14 -0600

    Add changes

--- main.py
+++ main.py
@@ -23,14 +23,7 @@ import os, sys, re, sqlite3
 
 DATABASE_NAME = "sistema_usuarios.db"
 usuarios = {}
-vacantes_ccc = [
- {"id_vacante": 1, "campana": "Soporte Técnico", "idioma": "Inglés", "salario_min":
-15000},
- {"id_vacante": 2, "campana": "Ventas Outbound", "idioma": "Español", "salario_min":
-8000},
- {"id_vacante": 3, "campana": "Atención a Clientes VIP", "idioma": "Inglés", "salario_min":
-18000}
-]
+
 def registrar_candidato(correo):
     """Registra un nuevo usuario en el sistema solicitando sus requerimientos mínimos."""
     print("\n--- REGISTRO DE CANDIDATO ---")
@@ -74,7 +67,7 @@ def registrar_candidato(correo):
                 
                 for email, info in usuarios.items():
                     datos_registro = (
-                        email,  # Usamos 'email' del diccionario, no la variable global de arriba
+                        email,
                         info['nombre'], 
                         info['idioma'], 
                         info['salario_esperado'],
@@ -86,6 +79,7 @@ def registrar_candidato(correo):
                 print("*** Usuario guardado exitosamente en la Base de Datos ***")
     else:
         print("NO se ingresó un correo válido")
+    
     
 def consultar_perfil(driver):
     """Permite al usuario ver sus datos registrados."""
@@ -110,6 +104,7 @@ def consultar_perfil(driver):
 
 
 def modificar_perfil(driver):
+    """Cambiar la expectativa salarial minima"""
     nuevo_salario = float(input("Nueva expectativa salarial mínima: "))
     search_btn = driver.find_element(By.CSS_SELECTOR, '[data-testid="nav-search"]')
     search_btn.click()
@@ -135,8 +130,8 @@ def modificar_perfil(driver):
     boton_guardar.click()
     print(f"Salario cambiado con éxito a: {nuevo_salario}")
     print("¡Perfil actualizado correctamente!")
-
         
+    
 def eliminar_perfil(correo):
     """Elimina el registro del usuario del sistema."""
     try:
@@ -148,28 +143,107 @@ def eliminar_perfil(correo):
         print(f"El usuario con correo {correo} ha sido eliminado.")
     except Exception as e:
         print(F"No se encontró ningún usuario con el correo {correo}.")
+
+
+def excel_has_roles_to_search(excel_file_path:str):
+    """Obtener datos filtrados"""
+    # Search if exist excel input for to search roles in OCC 
+    ls_roles_to_search = []
+    
+    wb = openpyxl.load_workbook(filename=excel_file_path)
+    sh = wb["vacantes"]    
+    
+    for row in sh.iter_rows(values_only=True):
+        if not row[0] == "Nombre de la vacante a buscar" and not row[0] is None:
+            ls_roles_to_search.append(str(row[0]).strip())
+    
+    if bool(ls_roles_to_search):
+        return ls_roles_to_search
+    return []
         
-def buscar_vacantes_match():
+        
+def buscar_vacantes_match(excel_file_path:str, driver, usuario):
     """Cruza el perfil del usuario con las vacantes para mostrar solo las viables."""
-    correo = input("\nIngresa tu correo para buscar vacantes (Match): ")
-    if correo in usuarios:
-        perfil = usuarios[correo]
-        print(f"\nBuscando vacantes para {perfil['nombre']}...")
-        encontro_match = False
+    search_btn = driver.find_element(By.CSS_SELECTOR, '[data-testid="nav-search"]')
+    search_btn.click()
+    
+    # Search if exist excel input for to search roles in OCC 
+    ls_roles_to_search = []
+    
+    wb = openpyxl.load_workbook(filename=excel_file_path)
+    sh = wb["vacantes"]    
+    
+    ls_roles_to_search = excel_has_roles_to_search(excel_file_path=excel_file_path)
+    if not bool(ls_roles_to_search):
+        raise Exception(f"Especificar las vacantes que desea buscar por nombre o palabra clave en archivo '{excel_file_path}'")
 
-    # Ciclo para iterar sobre las vacantes disponibles
-    for vacante in vacantes_ccc:
-        # Condicional para validar compatibilidad (El núcleo del Smart Matching)
-        if vacante["idioma"] == perfil["idioma"] and vacante["salario_min"] >= perfil["salario_esperado"]:
-            print(f"-> MATCH ENCONTRADO: {vacante['campana']} | Salario: ${vacante['salario_min']} | Idioma: {vacante['idioma']}")
-            encontro_match = True
+    # Search Roles position in OCC search input
+    for role in ls_roles_to_search:
+        search_role_input = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.ID, "prof-cat-search-input-desktop")))
+        search_role_input.send_keys(Keys.CONTROL, "a")
+        search_role_input.send_keys(Keys.BACKSPACE)
 
-    if not encontro_match:
+        search_role_input.send_keys(role.lower())
+        search_role_input.send_keys(Keys.ENTER) 
+        sleep(4)
+        
+        all_jobs_search = driver.find_elements(By.CSS_SELECTOR, "div[data-offers-grid-offer-item-container]")
+
+        if bool(all_jobs_search):
+            print("********** EMPLEOS ENCONTRADOS **********")
+            for job_pos in all_jobs_search:
+                try:
+                    cover_salary = False
+                    cover_language = False
+                    
+                    # Get info job position
+                    job_name = job_pos.find_element(By.TAG_NAME, "h2").text.strip()
+                    print(f" - {job_name}")
+                    job_full_info = driver.find_element(By.XPATH, f"//aside//div[contains(@class, 'card-job-offer')]//h2[contains(text(), '{job_name}')]")
+                    job_full_info.click()
+                    sleep(2)
+                    job_pos = driver.find_element(By.ID, "job-detail-container").text
+                    
+                    try:
+                        # Get salary
+                        salary = re.findall(r"(\d[\d,.]*)\s*(?:-|–|—|[aA]\b|al\b|hasta\b)\s*(?:[^0-9\n]*)\s*(\d[\d,.]*)", job_pos)
+                        min_salary = salary[0][0]
+                        max_salary = salary[0][1]
+                        
+                        print(f"- Salario minimo {min_salary}\n- Salario maximo {max_salary}")
+                        
+                        if min_salary >= usuario["salario_esperado"]:
+                            cover_salary = True
+                    except:
+                        print("Salario no mostrado")
+                    
+                    # Get languaje
+                    if usuario["idioma"].lower() == "ingles":
+                        english_pos = re.findall(r"(ingles|english|working in the United States)", job_pos, re.IGNORECASE)
+                        if english_pos:
+                            cover_language = True
+
+                    else:
+                        cover_language = True
+                        
+                    if cover_language and cover_language:
+                        print("POSTULARSE")
+                        
+                    else:
+                        print("NO POSTULARSE: no se cumplen los requisitos")
+                    
+                except StaleElementReferenceException:
+                    continue
+            print("===============================================")
+    
+    if True:
         print("Por el momento no hay vacantes que cumplan con tus requisitos exactos.")
     else:
         print("Debes estar registrado para buscar vacantes.")
 
+
 def logging_page(driver:webdriver, user:str, password:str):
+    """Ingresar a OCC mundial"""
     try:
         page_load = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "login-link")))        
         if bool(page_load.text):
@@ -196,11 +270,15 @@ def logging_page(driver:webdriver, user:str, password:str):
     except Exception as e:
         print(f"Error: {e}")
         return False
+
         
 def open_web_page(driver:webdriver, url_web_page:str):
+    """Abrir Navegador"""
     driver.get(url_web_page)
 
+
 def excel_search_role_pos(excel_file_path:str):
+    """Crear excel para validar puestoa a aplicar"""
     try:
         # Validate if excel file exists
         if not os.path.isfile(excel_file_path):
@@ -215,28 +293,9 @@ def excel_search_role_pos(excel_file_path:str):
         print(f"Error: {e}")
         return False
 
-def excel_has_roles_to_search(excel_file_path:str):
-    # Search if exist excel input for to search roles in OCC 
-    ls_roles_to_search = []
-    
-    wb = openpyxl.load_workbook(filename=excel_file_path)
-    sh = wb["vacantes"]    
-    
-    for row in sh.iter_rows(values_only=True):
-        if not row[0] == "Nombre de la vacante a buscar" and not row[0] is None:
-            ls_roles_to_search.append(str(row[0]).strip())
-    
-    if bool(ls_roles_to_search):
-        return ls_roles_to_search
-    return []
-
-def search_role_pos_in_occ(roles_to_search:list[str]):
-    pass
 
 def obtener_datos_usuario(correo):
-    """
-    Busca un usuario por su correo en la base de datos.
-    """
+    """Busca un usuario por su correo en la base de datos."""
     try:
         with sqlite3.connect(DATABASE_NAME) as conn:
             cursor = conn.cursor()
@@ -275,6 +334,10 @@ def menu_principal():
     # Load variables
     url_occ_mundial  = os.getenv("URL_OCC")
     excel_file       = os.getenv("EXCEL_FILE")
+
+    # Ruta origen
+    base_path = str(Path(__file__).parent)
+    excel_file_path = os.path.join(base_path, excel_file)
 
     opcion = ""
     while opcion != "5":
@@ -320,19 +383,22 @@ def menu_principal():
                 # Enter to the web page
                 open_web_page(driver=driver, url_web_page=url_occ_mundial)            
                 logging_result = logging_page(driver=driver, user=usuario["correo"], password=usuario["password"])
-                    
-                if opcion == "1":
-                    consultar_perfil(driver)
-                elif opcion == "2":
-                    modificar_perfil(driver)
-                elif opcion == "3":
-                    eliminar_perfil(mail_access)
-                elif opcion == "4":
-                    buscar_vacantes_match()
-                elif opcion == "5":
-                    print("Saliendo del sistema...")
                 
+                if logging_result:
+                    if opcion == "1":
+                        consultar_perfil(driver)
+                    elif opcion == "2":
+                        modificar_perfil(driver)
+                    elif opcion == "3":
+                        eliminar_perfil(mail_access)
+                    elif opcion == "4":
+                        buscar_vacantes_match(excel_file_path, driver, usuario)
+                    elif opcion == "5":
+                        print("Saliendo del sistema...")
+                else:
+                    print("No se pudo ingresar a OCC")
                 driver.close()
+                driver.quit()
                 
         else:
             print("Opción no válida. Intenta de nuevo.")
